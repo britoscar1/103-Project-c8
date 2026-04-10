c67253934ff93346e1efcbfda7114875d036481b
Oscar Brito <brito.oscar@uabc.edu.mx>
2026-04-09 17:20:11 -0700

    Initial Commit

---
+++ 103-Project-c8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103-Project-c8
+//
+//  Created by Oscar Brito on 09/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103-Project-c8/_03_Project_c8App.swift
@@ -0,0 +1,17 @@
+//
+//  _03_Project_c8App.swift
+//  103-Project-c8
+//
+//  Created by Oscar Brito on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct _03_Project_c8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
